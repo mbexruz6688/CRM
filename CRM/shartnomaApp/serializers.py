dd8b1826beab9b6972290825cb45e61b83e7af6f
fr1day <bmahmudov321@gmail.com>
2021-12-28 17:57:28 +0500

    Updating serializer.py!

--- CRM/shartnomaApp/serializers.py
+++ CRM/shartnomaApp/serializers.py
@@ -6,3 +6,23 @@ class KursSerializer(ModelSerializer):
     class Meta:
         model = Kurs
         fields = ["nom", "narx"]
+
+
+class UstozSerializer(ModelSerializer):
+    class Meta:
+        model = Ustoz
+        fields = ["ism", "yosh", "tel"]
+
+
+class StudentSerializer(ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ["ism", "familiya", "sharif", "jins", "tel1", "tel2", "status"]
+
+
+class ShartnomaSerializer(ModelSerializer):
+    class Meta:
+        model = Shartnoma
+        fields = "__all__"
+
+
