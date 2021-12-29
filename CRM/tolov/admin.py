4b54a7893bce27b3fc387b61b2c8dd2757eeb633
fr1day <bmahmudov321@gmail.com>
2021-12-29 10:11:40 +0500

    Created new app!

---
+++ CRM/tolov/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from tolov.models import Tolov
+
+admin.site.register(Tolov)
---
+++ CRM/tolov/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import SET_NULL
+from shartnomaApp.models import *
+
+class Tolov(models.Model):
+    years = ( 
+    ("2022", "2022"), 
+    ("2023", "2023"), 
+    ("2024", "2024"), 
+    ("2025", "2025"),
+    ) 
+
+    months = ( 
+    ("January", "January"), 
+    ("February", "February"), 
+    ("March", "March"), 
+    ("April", "April"), 
+    ("May", "May"), 
+    ("June", "June"), 
+    ("Jule", "Jule"), 
+    ("August", "August"), 
+    ("September", "September"), 
+    ("October", "October"), 
+    ("November", "November"), 
+    ("December", "December"),  
+    )
+    shartnoma = models.ForeignKey(Shartnoma, on_delete=models.SET_NULL, null=True)
+    ustoz = models.ForeignKey(Ustoz, on_delete=SET_NULL, null=True)
+    student = models.ForeignKey(Student, on_delete=SET_NULL, null=True)
+    yil = models.IntegerField()
+    tolov_oyi = models.CharField(max_length=20)
+    chegirma_status = models.CharField(max_length=10, choices=years)
+    ch_sababi = models.CharField(max_length=50)
+    ch_miqdori = models.PositiveBigIntegerField()
+    t_sanasi = models.DateField()
+    kassir = models.CharField(max_length=30)
+    tolashi_lozim = models.PositiveBigIntegerField()
+    naqd = models.PositiveBigIntegerField()
+    bank = models.PositiveBigIntegerField()
+    plastik = models.PositiveBigIntegerField()
+    click = models.PositiveBigIntegerField()
+    jami_tolandi = models.PositiveBigIntegerField()
+    jami_qoldi = models.PositiveBigIntegerField()
